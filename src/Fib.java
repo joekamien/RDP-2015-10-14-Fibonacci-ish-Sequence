1a889e8db89f4df707ae63bc2a12dcbe2c53530f
joekamien <joekamien@users.noreply.github.com>
2015-10-14 10:27:50 -0700

    Update Fib and Tester for bigint inputs

--- src/Fib.java
+++ src/Fib.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 
 public class Fib {
 	
@@ -49,6 +50,59 @@ public class Fib {
 		}while (current <= target);
 		
 		return ret.trim();
+	}
+	
+	public static String sequence(BigInteger target){
+		if (target.compareTo(new BigInteger("2147483647")) < 0) return sequence(target.intValue());
+		
+		//this actually does need to be handled as a big integer
+		
+		BigInteger valat1 = Fib.findLowestValAt1(target);
+		String ret = "0 " + valat1 + " ";
+		if (target.equals(valat1)) return ret.trim();
+		
+		BigInteger valat0 = BigInteger.ZERO;
+		BigInteger current = valat0.add(valat1);
+		do{
+			ret += current + " ";
+			valat0 = valat1;
+			valat1 = current;
+			current = valat0.add(valat1);
+		}while (current.compareTo(target) <= 0);
+		
+		return ret.trim();
+	}
+	
+	//Handle BigInteger situation
+	public static boolean contains(BigInteger valat1, BigInteger target){
+		if(target.compareTo(BigInteger.ZERO) == 0 || target.compareTo(valat1) == 0) return true;
+		if(valat1.compareTo(BigInteger.ZERO) <= 0 || target.compareTo(BigInteger.ZERO) < 0) throw new IllegalArgumentException("Value at 1 and target must be positive");
+		
+		BigInteger valat0 = BigInteger.ZERO;
+		
+		while(true){
+			BigInteger current = valat0.add(valat1);
+			if(current.compareTo(target) == 0){
+				return true;
+			}
+			else if(current.compareTo(target) > 0){
+				return false;
+			}
+			
+			//increment for next iteration
+			valat0 = valat1;
+			valat1 = current;
+		}
+	}
+	
+	public static BigInteger findLowestValAt1(BigInteger target){
+		for(BigInteger i = BigInteger.ONE; i.compareTo(target) < 0; i = i.add(BigInteger.ONE)){
+			if(Fib.contains(i, target)){
+				return i;
+			}
+		}
+		
+		return target;
 	}
 	
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.math.BigInteger;
+
 import org.junit.Test;
 
 @SuppressWarnings("unused")
@@ -13,11 +15,24 @@ public class Tester {
 		assert(Fib.contains(1, 9) == false);
 		assert(Fib.findLowestValAt1(9) == 3);
 		assert(Fib.findLowestValAt1(84) == 4);
+		
+		//test for biginteger
+		
+		assert(Fib.contains(BigInteger.ONE, new BigInteger("2")));
+		assert(Fib.contains(new BigInteger("2000"), BigInteger.ZERO));
+		assert(Fib.contains(new BigInteger("3"), new BigInteger("9")));
+		assert(Fib.contains(new BigInteger("1"), new BigInteger("9")) == false);
+		assert(Fib.findLowestValAt1(new BigInteger("9")).compareTo(new BigInteger("3")) == 0);
+		assert(Fib.findLowestValAt1(new BigInteger("84")).compareTo(new BigInteger("4")) == 0);
+		
+		//printouts
 		System.out.println(Fib.sequence(9));
 		System.out.println(Fib.sequence(21));
 		System.out.println(Fib.sequence(84));
 		System.out.println(Fib.sequence(578));
 		System.out.println(Fib.sequence(123456789));
+		System.out.println(Fib.sequence(new BigInteger("38695577906193299")));
+		
 	}
 
 }
